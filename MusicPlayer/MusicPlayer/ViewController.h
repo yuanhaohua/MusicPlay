92618b19bf2c17b4a431eaf0f5240fbb893a7680
yuanhaohua <yuanhaohua123@qq.com>
2016-05-04 18:35:28 +0800

    袁好华+创建工程

---
+++ MusicPlayer/MusicPlayer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MusicPlayer
+//
+//  Created by yuan on 16/5/4.
+//  Copyright © 2016年 yuan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
