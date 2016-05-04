baa69155ea27fb2be6acee4383390b011fd37a80
lkjy <2748517089@qq.com>
2016-05-04 19:05:40 +0800

    ll

--- MusicPlayer/MusicPlayer/DFMUpchannelsEntity.h
+++ MusicPlayer/MusicPlayer/DFMUpchannelsEntity.h
@@ -4,10 +4,12 @@
 //
 //  Created by lk on 16/5/4.
 //  Copyright © 2016年 yuan. All rights reserved.
-//
+//wangjihongh 
 
 #import <Foundation/Foundation.h>
 
 @interface DFMUpchannelsEntity : NSObject
+
 @property(nonatomic,copy)NSMutableArray*channels;
+
 @end
